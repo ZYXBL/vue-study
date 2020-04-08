337b5a1fc92ad92c1853923065fae4372c5252ba
周阳 <zy825692497@163.com>
2020-04-08 10:53:38 +0800

    ssr使用vue-router接管路由

--- server/express.js
+++ server/express.js
@@ -5,6 +5,10 @@ const app = express();
 
 // 导入vue
 const Vue = require('vue');
+// 导入vue-router
+const Router = require('vue-router');
+// 使用router实例
+Vue.use(Router);
 
 // 渲染器导入
 const { createRenderer } = require('vue-server-renderer');
@@ -12,17 +16,41 @@ const { createRenderer } = require('vue-server-renderer');
 // 创建一个渲染器
 const renderer = createRenderer();
 
-app.get('/', async function(req, res) {
+app.get('*', async function(req, res) {
   // res.send('hello world')
+  
+  // 创建一个router实例
+  const router = new Router({
+    mode: 'history',
+    routes: [
+      {path: '/', component: { template: '<div>index page</div>' }},
+      {path: '/detail', component: { template: '<div>detail page</div>' }}
+    ]
+  });
 
   // 创建一个Vue实例
   const vm = new Vue({
     data: {
       name: 'vue ssr渲染',
     },
-    template: '<div>{{name}}</div>'
+    template: `
+      <div>
+        <div @click="onClick">{{name}}</div>
+        <router-link to="/">index</router-link>
+        <router-link to="/detail">detail</router-link>
+        <router-view></router-view>
+      </div>
+    `,
+    router,
+    methods: {
+      OnClick() {
+        console.log('hello')
+      }
+    }
   });
   try {
+    // 跳转至url对应路由页面
+    router.push(req.url);
     const html = await renderer.renderToString(vm);
     // 将渲染html字符串返回给客户端
     res.send(html);
